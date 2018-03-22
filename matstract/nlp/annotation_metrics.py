871878c178cc4b222aca10b52dba17f5d5f17029
vtshitoyan <tsh.vahe@gmail.com>
2018-03-22 14:13:10 -0700

    added tests for annotation_metrics

--- matstract/nlp/annotation_metrics.py
+++ matstract/nlp/annotation_metrics.py
@@ -13,19 +13,29 @@ class AnnotationMetrics:
     def __init__(self, local=False):
         self._db = open_db_connection(access="annotator", local=local)
 
-    def similarity_matrix(self, labels=None, annotators=None):
+    def similarity_matrix(self, labels=None, annotators=None, pairs=None):
         """
         Computes a similarity matrix across different annotators for different labels to
         give an idea which labels are the most similar ones, so maybe they can be merged or re-defined
-        :param labels:      A list of labels to consider. If None, all occurring labels will
+        :param labels:      A list or set of labels to consider. If None, all occurring labels will
                             be considered, including the None label
         :param annotators:  A list of user_keys to consider. If none, all users will be considered.
+        :param pairs:       You can specify the pairs of annotations manually if you don't want to connect to the db
+                            [{"common_labels": ['MAT', ..], "ann_list": [['DSC', 'MAT', ..], ['DSC', 'CHM', ..]]}, ..]
         :return:            A numpy array as the similarity matrix, and an ordered list of existing labels
         """
-        pairs, existing_labels = self._annotation_pairs(labels=labels, annotators=annotators)
+        if pairs is None:
+            pairs, existing_labels = self._annotation_pairs(labels=labels, annotators=annotators)
+        else:
+            existing_labels = set()
+            for pair in pairs:
+                # need to compute the existing labels if the pairs are manually supplied
+                existing_labels = set().union(existing_labels, set(pair["ann_list"][0]), set(pair["ann_list"][1]))
 
         if labels is None:
-            labels = existing_labels
+            labels = list(existing_labels)
+        else:
+            labels = list(labels)
 
         nr_labels = len(labels)
         similarity_matrix = np.zeros([nr_labels, nr_labels])
@@ -39,26 +49,41 @@ class AnnotationMetrics:
 
         return similarity_matrix, labels
 
-    def cohen_kappa(self, labels=None, annotators=None):
+    def cohen_kappa(self, labels=None, annotators=None, pairs=None):
         """
         Computes cohen kappa for the given set of labels for all annotators pair-wise
         and returns the mean across all annotator pairs. Also returns the used labels.
         This is the overall metric of the agreement between annotators across the labels.
-        :param labels:      A list of labels to consider. If None, all occurring labels will
+        :param labels:      A set of labels to consider. If None, all occurring labels will
                             be considered, including the None label
         :param annotators:  A list of user_keys to consider. If none, all users will be considered.
-        :return:            mean of kappa, its standard deviation and existing labels
+        :param pairs:       You can specify the pairs of annotations manually if you don't want to connect to the db
+                            [{"common_labels": ['MAT', ..], "ann_list": [['DSC', 'MAT', ..], ['DSC', 'CHM', ..]]}, ..]
+        :return:            mean of kappa, its standard deviation and existing labels (set)
         """
-        pairs, existing_labels = self._annotation_pairs(labels=labels, annotators=annotators)
-        kappas = []
-        if labels is None:
-            labels = existing_labels
+        existing_labels = set()
+        pairs_supplied = True
+        if pairs is None:
+            pairs_supplied = False
+            pairs, existing_labels = self._annotation_pairs(labels=labels, annotators=annotators)
 
+        kappas = []
         for pair in pairs:
+            # need to compute the existing labels if the pairs are manually supplied
+            if pairs_supplied:
+                existing_labels = set().union(existing_labels, set(pair["ann_list"][0]), set(pair["ann_list"][1]))
+
+            # if one has specified specific labels, need to exclude everything else from common labels
+            if labels is not None:
+                pair_labels = list(pair["common_labels"].intersection(set(labels)))
+            else:
+                pair_labels = list(pair["common_labels"])
+
+            # computing kappa for the given pair
             kappas.append(cohen_kappa_score(
                 pair["ann_list"][0],
                 pair["ann_list"][1],
-                labels=list(set(pair["common_labels"]).intersection(set(labels)))))
+                labels=pair_labels))
 
         return np.nanmean(kappas), np.nanstd(kappas), existing_labels
 
@@ -82,26 +107,26 @@ class AnnotationMetrics:
             similarity_matrix, labels = self.similarity_matrix(labels=labels, annotators=annotators)
 
         accuracies = dict()
-        for i, label in enumerate(labels):
-            accuracies[label] = similarity_matrix[i, i] / np.sum(similarity_matrix[i, :])
+        for i, label in enumerate(list(labels)):
+            accuracies[label] = similarity_matrix[i, i] / np.nansum(similarity_matrix[i, :])
 
         return accuracies
 
     def _annotation_pairs(self, labels=None, annotators=None):
         """
         Get's all annotation list pairs to be used for annotator agreement measures
-        :param labels:              A list of labels to consider. If None, all occurring labels will
+        :param labels:              A set or list of labels to consider. If None, all occurring labels will
                                     A be considered, including the None label
-        :param annotators:          A list of user_keys to consider. If none, all users will be considered.
-        :return:                    list of tuples representing pairs of annotations and existing labels
+        :param annotators:          A set of user_keys to consider. If none, all users will be considered.
+        :return:                    list of tuples representing pairs of annotations and existing labels (set)
         """
 
         # only the specified labels and annotators
         constraints = dict()
         if labels is not None:
-            constraints["labels"] = {"$in": labels}
+            constraints["labels"] = {"$in": list(labels)}
         if annotators is not None:
-            constraints["user"] = {"$in": annotators}
+            constraints["user"] = {"$in": list(annotators)}
 
         relevant_annotations = getattr(self._db, AnnotationBuilder.ANNOTATION_COLLECTION).find(constraints)
 
@@ -121,8 +146,8 @@ class AnnotationMetrics:
         for doi in ann_groups:
             if len(ann_groups[doi]) > 1:
                 for pair in itertools.combinations(ann_groups[doi], r=2):
-                    common_labels = list(set(pair[0]["labels"]).intersection(set(pair[1]["labels"])).union({str(None)}))
+                    common_labels = set(pair[0]["labels"]).intersection(set(pair[1]["labels"])).union({str(None)})
                     existing_labels = set().union(existing_labels, set(pair[0]["list"]), set(pair[1]["list"]))
                     pairs.append({"common_labels": common_labels, "ann_list": [p["list"] for p in pair], "doi": doi})
 
-        return pairs, list(existing_labels)
+        return pairs, existing_labels
--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     author_email='jdagdelen@berkeley.edu',
     description='Materials extraction from scientific abstracts.',
     url='https://github.com/materialsintelligence/matstract',
-    download_url = 'https://github.com/materialsintelligence/matstract/archive/0.0.tar.gz'
+    download_url = 'https://github.com/materialsintelligence/matstract/archive/0.0.tar.gz',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
 )
