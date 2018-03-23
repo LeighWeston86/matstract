f5655c4b2fa96e2305c1bfc5857f173c81822a45
jdagdelen <jdagdelen@lbl.gov>
2018-03-23 15:16:49 -0700

    Similar app initial commit.

--- matstract/web/view/similar_app.py
+++ matstract/web/view/similar_app.py
@@ -1,18 +1,37 @@
 import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
+import random
 from matstract.utils import open_db_connection, open_es_client
 from matstract.extract import parsing
-from elasticsearch_dsl import Search
-from elasticsearch_dsl.query import Match, MultiMatch
 from bson import ObjectId
 
-db = open_db_connection()
+db = open_db_connection(db = "matstract_db")
 client = open_es_client()
+
+def random_abstract():
+    count = db.abstracts.count()
+    random_document = db.abstracts.find()[random.randrange(count)]
+    return random_document['abstract']
+
 
 def sort_results(results, ids):
     results_sorted = sorted(results, key=lambda k: ids.index(k['_id']))
     return results_sorted
+
+
+def highlight_material(body, material):
+    highlighted_phrase = html.Mark(material)
+    if len(material) > 0 and material in body:
+        chopped = body.split(material)
+        newtext = []
+        for piece in chopped[:-1]:
+            newtext.append(piece)
+            newtext.append(highlighted_phrase)
+        newtext.append(chopped[-1])
+        return newtext
+    return body
+
 
 def get_search_results(search="", material="", max_results=10000):
     results = None
@@ -27,29 +46,33 @@ def get_search_results(search="", material="", max_results=10000):
     if material and not search:
         results = db.abstracts_leigh.find({"normalized_cems": parser.matgen_parser(material)})
     elif search and not material:
-        ids = elastic_search(search, max_results)
-        results = sort_results(db.abstracts.find({"_id":{"$in": ids[0:1000]}}), ids)
+        ids = find_similar(search, max_results)
+        results = sort_results(db.abstracts.find({"_id": {"$in": ids[0:1000]}}), ids)
     elif search and material:
-        ids = elastic_search(search, max_results)[0:1000]
+        ids = find_similar(search, max_results)[0:1000]
         results = db.abstracts_leigh.aggregate([
-            {"$match": {"_id": {"$in":ids}}},
+            {"$match": {"_id": {"$in": ids}}},
             {"$match": {"normalized_cems": parser.matgen_parser(material)}}
         ])
     return list(results)
 
 
-def elastic_search(search="", max_results=10000):
-    if search is None:
-        search = ''
-    if search == '':
+def find_similar(abstract="", max_results=10000):
+    if abstract is None or abstract == '':
         return None
 
-    query = {"query": {"simple_query_string": {"query": search}}}
+    query = {"query": {
+            "more_like_this" : {
+                "fields" : ['title', 'abstract'],
+                "like" : abstract
+                }
+            }}
 
-    # hits = client.search(index="tri_abstracts", body=query, _source_include=["id"], size=max_results)["hits"]["hits"]
     hits = client.search(index="tri_abstracts", body=query, size=max_results)["hits"]["hits"]
     ids = [ObjectId(h["_id"]) for h in hits]
+    print(len(ids))
     return ids
+
 
 def to_highlight(names_list, material):
     parser = parsing.SimpleParser()
@@ -57,6 +80,13 @@ def to_highlight(names_list, material):
     for name in names_list:
         if 'names' in name.keys() and parser.matgen_parser(name['names'][0]) == parser.matgen_parser(material):
             return name['names'][0]
+
+
+def sort_df(test_df, materials):
+    test_df['to_highlight'] = test_df['chem_mentions'].apply(to_highlight, material=materials)
+    test_df['count'] = test_df.apply(lambda x: x['abstract'].count(x['to_highlight']), axis=1)
+    test_df.sort_values(by='count', axis=0, ascending=False, inplace=True)
+    return test_df
 
 
 def generate_table(search='', materials='', columns=('title', 'authors', 'year', 'abstract'), max_rows=100):
@@ -82,47 +112,36 @@ def generate_table(search='', materials='', columns=('title', 'authors', 'year',
             # Body
             [html.Tr([
                 html.Td(html.A(hm(str(df.iloc[i][col]), df.iloc[i]['to_highlight'] if materials else search),
-                               href=df.iloc[i]["html_link"], target="_blank")) if col == "title"
-                else html.Td(
-                    hm(str(df.iloc[i][col]), df.iloc[i]['to_highlight'] if materials else search)) if col == "abstract"
+                               href=df.iloc[i]["link"], target="_blank")) if col == "title"
                 else html.Td(df.iloc[i][col]) for col in columns])
                 for i in range(min(len(df), max_rows))]
         )
     return html.Table("No Results")
 
-# The Search app
+
+# The Similar app
 layout = html.Div([
     html.Div([
         html.Div([
-            html.P('Welcome to the Matstract Database!')
+            html.P('Matstract Doppelgängers: find similar abstracts.')
         ], style={'margin-left': '10px'}),
-
-        html.Label('Search for similar abstracts:):'),
-        dcc.Textarea(id='search-box',
-                     autoFocus=True,
-                     spellCheck=True,
-                     wrap=True,
-                     style={"width": "100%"},
-                     placeholder='Search: e.g. "Li-ion battery"'),
-    ]),
-
-    html.Div([
-        dcc.Input(id='material-box',
-                  placeholder='Material: e.g. "LiFePO4"',
-                  type='text'),
-        html.Button('Submit', id='search-button'),
-    ]),
-    # Row 2:
-    html.Div([
-
+        html.Label('Enter an abstract to find similar entries:'),
+        html.Div(dcc.Textarea(id='similar-textarea',
+                              style={"width": "100%"},
+                              autoFocus=True,
+                              spellCheck=True,
+                              wrap=True,
+                              placeholder='Paste abstract/other text here.'
+                              )),
         html.Div([
-
-        ], className='nine columns', style=dict(textAlign='center')),
-
-    ], className='row'),
-
-    html.Div([
-        html.Label(id='number_results'),
-        html.Table(id='table-element')
-    ], className='row', style={"overflow": "scroll"})
+            dcc.Input(id='similar-material-box',
+                      placeholder='Material: e.g. "LiFePO4"',
+                      type='text')
+        ]),
+        html.Div([html.Button('Find Doppelgängers', id='similar-button'),
+                  html.Button('Choose a random abstract', id='similar-random')]),
+        html.Div([
+            html.Table(id='similar-table')
+        ], className='row', style={"overflow": "scroll"}),
+    ], className='twelve columns'),
 ])
