bd62d879f1a0a2c9058a1a8f3640ed543b98317b
vtshitoyan <tsh.vahe@gmail.com>
2018-05-23 17:19:04 -0700

    removed debug msg

--- matstract/models/database.py
+++ matstract/models/database.py
@@ -37,8 +37,7 @@ class AtlasConnection():
                 db_creds = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config/db_creds.json')
                 user_creds = json.load(open(db_creds, "r"))["mongo"][access][db]
                 uri = "mongodb://{user}:{pass}@{rest}".format(**user_creds)
-            except Exception as e:
-                print(str(e))
+            except:
                 if db=="production":
                     db = "matstract_db"
                 elif db == "test":
