9fb4cb79cb4fb7c2156b8502a91980d7678fc80e
vtshitoyan <tsh.vahe@gmail.com>
2018-10-02 18:23:52 -0700

    cluster plot, materials search and materials map app changes

--- matstract/web/front.py
+++ matstract/web/front.py
@@ -7,8 +7,8 @@ import dash_materialsintelligence as dmi
 from flask import send_from_directory
 
 from dash.dependencies import Input, Output
-from matstract.web.view import new_search_app, summary_app, matsearch_app
-from matstract.web.callbacks import new_search_callbacks, summary_callbacks, matsearch_callbacks
+from matstract.web.view import new_search_app, summary_app, matsearch_app, material_map_app
+from matstract.web.callbacks import new_search_callbacks, summary_callbacks, matsearch_callbacks, material_map_callbacks
 from matstract.models.database import AtlasConnection
 
 # app config
@@ -53,7 +53,7 @@ nav = html.Nav(
             html.Span(u" \u2022 "),
             dcc.Link("Material Search", href="/matsearch"),
             html.Span(u" \u2022 "),
-            html.A("Materials Map", href="https://plot.ly/~vtshitoyan/6/markers-and-text/#/"),
+            dcc.Link("Materials Map", href="/material_map"),
         ],
         id="nav_bar")
 
@@ -73,9 +73,11 @@ def display_page(path):
     if path.startswith("/search"):
         return new_search_app.serve_layout(path)
     elif path == "/summary":
-         return summary_app.layout
+        return summary_app.layout
     elif path == "/matsearch":
-         return matsearch_app.serve_layout(db)
+        return matsearch_app.serve_layout(db)
+    elif path == "/material_map":
+        return material_map_app.layout
     else:
         return new_search_app.serve_layout()
 
@@ -90,3 +92,4 @@ def get_stylesheet(path):
 new_search_callbacks.bind(app)
 summary_callbacks.bind(app)
 matsearch_callbacks.bind(app)
+material_map_callbacks.bind(app)
--- matstract/web/view/material_map_app.py
+++ matstract/web/view/material_map_app.py
@@ -1,30 +1,46 @@
 import dash_html_components as html
 import dash_core_components as dcc
-import operator
-import numpy as np
-from urllib.request import urlopen
-import json
+from matstract.models.cluster_plot import ClusterPlot
 
-from matstract.models.word_embeddings import EmbeddingEngine
-import plotly.graph_objs as go
 
-ee = EmbeddingEngine()
-embs = ee.embeddings / ee.norm
+cp = ClusterPlot()
+plot_data = cp.get_plot_data(entity_type="materials", limit=-1, heatphrase=None, wordphrases=None)
+
+# ee = EmbeddingEngine()
+# embs = ee.embeddings / ee.norm
+# # ds = np.DataSource()
+#
+# # # loading tsne matrix
+# # tsne_matrix_url = "https://s3-us-west-1.amazonaws.com/matstract/material_map_tsne_5.npy"
+# # ds.open(tsne_matrix_url)
+# # tsne_matrix = np.load(ds.abspath(tsne_matrix_url))
+#
 # ds = np.DataSource()
+# material_names_url = "https://s3-us-west-1.amazonaws.com/materialsintelligence/material_map_tsne_words.npy"
+# material_coords_url = "https://s3-us-west-1.amazonaws.com/materialsintelligence/final_material_map_atl10_30_ee12_lr200.npy"
+#
+# ds.open(material_names_url)
+# material_names = np.load(ds.abspath(material_names_url))
+# ds.open(material_coords_url)
+# material_coords = np.load(ds.abspath(material_coords_url))
 
-# # loading tsne matrix
-# tsne_matrix_url = "https://s3-us-west-1.amazonaws.com/matstract/material_map_tsne_5.npy"
-# ds.open(tsne_matrix_url)
-# tsne_matrix = np.load(ds.abspath(tsne_matrix_url))
+# material_names = np.load(data_root+"/data/material_map_tsne_words.npy").item()
+# material_map_tsne = np.load(data_root+"data/final_material_map_atl10_30_ee12_lr200.npy")
 
 
-response = urlopen("https://s3-us-west-1.amazonaws.com/matstract/material_map_10_mentions.json")
-data = response.read().decode("utf-8")
-tsne_data = json.loads(data)["data"][0]
-x = tsne_data["x"]
-y = tsne_data["y"]
-formulas = tsne_data["text"]
-formula_emb_indices = [ee.word2index[ee.dp.get_norm_formula(f)] for f in formulas]
+
+# response = urlopen("https://s3-us-west-1.amazonaws.com/matstract/material_map_10_mentions.json")
+# data = response.read().decode("utf-8")
+# tsne_data = json.loads(data)["data"][0]
+# x = tsne_data["x"]
+# y = tsne_data["y"]
+# formulas = tsne_data["text"]
+# formula_emb_indices = [ee.word2index[ee.dp.get_norm_formula(f)] for f in formulas]
+
+# x = material_coords[:, 0]
+# y = material_coords[:, 1]
+# formulas = material_names.item()
+# formula_emb_indices = [ee.word2index[f] if f in ee.word2index else None for f in formulas]
 
 # formula_counts = [0] * len(ee.formulas_full)
 # for i, formula in enumerate(ee.formulas_full):
@@ -46,7 +62,7 @@ formula_emb_indices = [ee.word2index[ee.dp.get_norm_formula(f)] for f in formula
 # x = tsne_matrix[formula_indices, 0]
 # y = tsne_matrix[formula_indices, 1]
 
-layout = {
+fig_layout = {
     'hovermode': 'closest',
     'showlegend': False,
     'height': 800,
@@ -68,26 +84,22 @@ layout = {
     }
 }
 
-data = [go.Scatter(
-    y=y,
-    x=x,
-    mode='markers',
-    text=formulas,
-    marker=dict(
-        size=5,
-        colorscale='Viridis',
-        showscale=False
-    ),
-    textposition="top"
-)]
-fig=dict(data=data)
-fig["layout"] = layout
+# data = [go.Scatter(
+#     y=plot_data["y"],
+#     x=plot_data["x"],
+#     mode='markers',
+#     text=plot_data["text"],
+#     marker=dict(
+#         size=5,
+#         colorscale='BuPu',
+#         showscale=False
+#     ),
+#     textposition="top"
+# )]
+# data = go.Scatter(plot_data)
+# print(data)
 
-graph = dcc.Graph(
-        id='material_map',
-        figure=fig,
-    )
-
+fig = dict(data=[plot_data], layout=fig_layout)
 layout = html.Div([
     html.Div([
         dcc.Input(id='map_keyword',
@@ -99,5 +111,8 @@ layout = html.Div([
             className="button-search",
             style={"display": "table-cell", "verticalAlign": "top"}),
     ], className="row"),
-    graph]
+    dcc.Graph(
+        id='material_map',
+        figure=fig,
+    )]
 )
